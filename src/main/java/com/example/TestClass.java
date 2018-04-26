a1f2a0c48842c8a1f023af3170a4abcf4aeb0171
deepakjoshi <deepak.joshi@impetus.co.in>
2018-04-26 18:58:38 +0530

    modified the test class

--- src/main/java/com/example/TestClass.java
+++ src/main/java/com/example/TestClass.java
@@ -1,5 +1,15 @@
 package com.example;
 
 public class TestClass {
+private final String message = "Hello World!";
 
+    public TestClass() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
 }
